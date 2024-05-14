dabbe2a0bf5de2facf0ca4ad444a95dd9dc2ef10
Nath Santos <nathsantos.ti@gmail.com>
2024-05-14 22:50:12 +0100

    Adicionando arquivos

---
+++ Readme.md
@@ -0,0 +1,42 @@
+# Aplicação de Previsão do Tempo ☀️⛈️
+
+Esta é uma aplicação web simples que permite aos usuários verificar a previsão do tempo de qualquer cidade do mundo. Ela utiliza a API do OpenWeatherMap para obter os dados meteorológicos em tempo real.
+
+## Funcionalidades
+
+- Pesquisar previsões do tempo por cidade e país.
+- Exibir informações detalhadas, incluindo temperatura atual, máxima e mínima, e ícone representando as condições climáticas.
+- Fundo de vídeo dinâmico que muda aleatoriamente para criar uma experiência visual interessante.
+- Funcionalidade de pesquisa ativada por tecla Enter e ao clicar no botão de pesquisa.
+
+## Como usar 🌍
+
+1. Digite o nome da cidade desejada no campo de busca.
+2. Se desejar, você pode incluir o nome do país separado por vírgula (por exemplo: "Paris, França").
+3. Pressione Enter ou clique no botão de pesquisa para ver a previsão do tempo.
+4. A previsão do tempo será exibida junto com um vídeo de fundo que muda aleatoriamente com novas pesquisas.
+
+## Visualização da Interface
+
+<center>
+![Interface da Aplicação de Previsão do Tempo](Previsão_Tempo.gif)
+</center>
+
+## Tecnologias Utilizadas 🛠️
+
+- HTML5
+- CSS3 (com animações e layout responsivo para dispositivos móveis)
+- JavaScript (incluindo fetch API para fazer solicitações HTTP)
+- API do OpenWeatherMap para obter dados meteorológicos
+- Vídeos de fundo dinâmicos para criar uma experiência visual envolvente
+
+## Próximos Passos 🚀
+
+- Implementar funcionalidade de previsão do tempo de vários dias.
+- Adicionar suporte para mais idiomas.
+- Melhorar a experiência do usuário com mais animações e interações.
+
+## Créditos 💡
+
+Este projeto foi desenvolvido por [Nathalia Santos] como parte de um compromisso contínuo de aprender e aprimorar habilidades de desenvolvimento web. Fique à vontade para contribuir, abrir issues ou fornecer feedback!
+
---
+++ src/js/script.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('load', () => {
+        alterarVideoBackground();
+    });  
+}) 
+
+    document.getElementById("input-busca").addEventListener('keyup', function(event){
+        if(event.keyCode === 13){
+            buscaTemp();
+            alterarVideoBackground();
+        }
+    });
+
+    function buscaTemp() {
+        var inputValor = document.getElementById("input-busca").value.trim().toLowerCase();
+        var [cidade, pais] = inputValor.split(',').map(item => item.trim()).map(item => removerAcentos(item).replace(/ç/g, 'c'));
+        var APIkey = "ee7e5dd0def56595006960b8910f8b9c"; 
+        var url;
+
+        if (pais) {
+            // Se o país foi especificado, use tanto o nome da cidade quanto o código do país na solicitação
+            var url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade},${pais}&appid=${APIkey}&units=metric&lang=pt`;
+        } else {
+            // Caso contrário, use apenas o nome da cidade na solicitação
+            var url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${APIkey}&units=metric&lang=pt`;
+        } 
+        
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Cidade não encontrada! Tente digitar (Cidade, País) para ser mais precisa a sua busca.");
+                }
+                return response.json();
+            })
+            .then(data => {
+                var temperaturaAtual = data.main.temp;
+                var temperaturaMaxima = data.main.temp_max;
+                var temperaturaMinima = data.main.temp_min;
+                var cidadeNome = data.name;
+                var climaIcone = data.weather[0].icon;
+                mostraTemp(cidadeNome, temperaturaAtual, temperaturaMaxima, temperaturaMinima, climaIcone);
+                limpaBusca(); 
+                mostraEnvelope(); 
+                alterarVideoBackground(); 
+            })
+            .catch(error => {
+                console.error("Erro ao obter dados!", error.message);
+                alert(error.message);
+            });
+    }
+
+    function mostraEnvelope() {
+        document.querySelector('.envelope').classList.add('mostrar'); 
+        document.querySelector('.caixa').style.alignItems = 'initial'; 
+        document.querySelector('.busca').classList.add('apos-busca');
+    }
+
+    function mostraTemp(cidadeNome, temperaturaAtual, temperaturaMaxima, temperaturaMinima, climaIcone) {
+        document.querySelector('.icone-tempo').src = `src/img/${climaIcone}.png`;
+        document.querySelector('.nome-cidade').innerHTML = `${cidadeNome}`;
+        document.querySelector('.temperatura').innerHTML = `${temperaturaAtual.toFixed(0)}°C`;
+        document.querySelector('.maxtemperatura').innerHTML = `Máx: ${temperaturaMaxima.toFixed(0)}°C`;
+        document.querySelector('.mintemperatura').innerHTML = `Mín: ${temperaturaMinima.toFixed(0)}°C`;
+    }
+
+    function limpaBusca() {
+        document.getElementById("input-busca").value = "";
+    }
+
+    function alterarVideoBackground() {
+        // Array com os caminhos dos vídeos
+        const videos = [
+            './src/video/video1.mp4',
+            './src/video/video2.mp4',
+            './src/video/video3.mp4',
+            './src/video/video4.mp4',
+            './src/video/video5.mp4',
+            './src/video/video6.mp4',
+            './src/video/video7.mp4',
+        ];
+
+        // Seleciona um vídeo aleatório do array
+        const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+
+        // Atualiza a origem do vídeo
+        document.getElementById("video-source").src = randomVideo;
+
+        // Recarrega o vídeo
+        document.querySelector(".video").load();
+    }
+
+    function removerAcentos(texto) {
+        return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    }
+
+
+
+
+
